ed879d3660e0cec92190b37bc6dcd0d924519415
BT_1N3_25 <sample@bths.edu>
2024-10-25 13:43:45 -0400

    New code added to Git

--- src/Git.java
+++ src/Git.java
@@ -3,5 +3,11 @@ public class Git {
         System.out.println("We love Git!!!");
         System.out.println("You are not awesome");
         System.out.println("No and bye");
+
+        int Hello2 = 0;
+        Hello2 += 3;
+        if (Hello2 == 3) {
+            System.out.println("We hate this house");
+        }
     }
 }
