21b3e0a4244965236fde4c71ac59800ad83354ab
BT_1N3_25 <sample@bths.edu>
2024-10-25 13:13:58 -0400

    changed print statements and added some new ones

--- src/GitRunner.java
+++ src/GitRunner.java
@@ -2,14 +2,18 @@ public class GitRunner {
 
 
     public static  void main(String[] args){
-        System.out.println("Hello World");
+        System.out.println("Bye world");
         System.out.println("This code is bad");
         int hello1 = 0;
-        hello1+=5;
+        hello1+=8;
         if (hello1 == 5) {
             System.out.println("Winner");
+        } else {
+            System.out.println("Loser");
         }
-
+        if (hello1 < 10) {
+            System.out.println("We love this world");
+        }
 
     }
 
