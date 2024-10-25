5d8875480c1f179806722d6e0365b84aad6573dd
BT_1N3_25 <sample@bths.edu>
2024-10-25 13:11:39 -0400

    added some new codes

--- src/GitRunner.java
+++ src/GitRunner.java
@@ -3,6 +3,14 @@ public class GitRunner {
 
     public static  void main(String[] args){
         System.out.println("Hello World");
+        System.out.println("This code is bad");
+        int hello1 = 0;
+        hello1+=5;
+        if (hello1 == 5) {
+            System.out.println("Winner");
+        }
+
+
     }
 
 
